8db0773450df0100dc8c7229a3cf79558a551728
aashka <136294443+aashka05@users.noreply.github.com>
2023-09-19 19:01:55 +0530

    Update prac4_5.cpp

--- practical4/prac4_5.cpp
+++ practical4/prac4_5.cpp
@@ -3,16 +3,16 @@
 
 void main()
 {
-    int i, n=0, sum=0, avg;
-    for (i = 1; i <= 100; i++)
-    {
-        if (i % 5 == 0)
-        {
-			      n = n + 1;
-			      sum = sum + i;
-            printf("%d\n", i);
-        }
-    }
-	  avg = sum / n;
-	  printf("Sum = %d, Average = %d\n", sum, avg);
+	int i, n=0, sum=0, avg;
+	for (i = 1; i <= 100; i++)
+	{
+	        if (i % 5 == 0)
+	        {
+		        n = n + 1;
+			sum = sum + i;
+	                printf("%d\n", i);
+	        }
+	}
+        avg = sum / n;
+	printf("Sum = %d, Average = %d\n", sum, avg);
 }
