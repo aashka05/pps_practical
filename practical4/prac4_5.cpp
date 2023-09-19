71e20117ae9b0c8c239ba01fdb69164a7d83900a
aashka <136294443+aashka05@users.noreply.github.com>
2023-09-19 18:58:20 +0530

    Create prac4_5.cpp

---
+++ practical4/prac4_5.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+
+void main()
+{
+    int i, n=0, sum=0, avg;
+    for (i = 1; i <= 100; i++)
+    {
+        if (i % 5 == 0)
+        {
+			      n = n + 1;
+			      sum = sum + i;
+            printf("%d\n", i);
+        }
+    }
+	  avg = sum / n;
+	  printf("Sum = %d, Average = %d\n", sum, avg);
+}
