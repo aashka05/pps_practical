949f48a134946d036bd67fe62dcfadc0463bf71d
aashka <136294443+aashka05@users.noreply.github.com>
2023-09-30 09:08:01 +0530

    Update prac4_04.cpp

--- practical4/prac4_04.cpp
+++ practical4/prac4_04.cpp
@@ -1,11 +1,25 @@
-// prac4_4.cpp : Defines the entry point for the console application.
-//
+#include <stdio.h>
+#include <conio.h>
 
-#include "stdafx.h"
-
-
-int _tmain(int argc, _TCHAR* argv[])
+void main()
 {
-	return 0;
-}
+    int x, y, i, n=0, sum=0;
+	float avg;
+	printf("Enter value of x: ");
+	scanf("%d", &x);
+	printf("Enter value of y: ");
+	scanf("%d", &y);
 
+	for (i = x; i <= y; i++)
+    {
+        if (i % 2 != 0)
+        {
+		sum = sum + i;
+            	printf("%d, %d\n", i, sum);
+		n += 1;
+        }
+    }
+	avg = (float)sum / n;
+	printf("Sum = %d, Average = %f\n", sum, avg);
+	getch();
+}
